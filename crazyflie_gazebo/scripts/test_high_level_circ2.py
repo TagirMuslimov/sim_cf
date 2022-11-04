1c19f069208a92e98a0faa6976fba3fa47f875d4
Kamil M <anbesp23@gmail.com>
2022-11-04 11:16:16 +0500

    Add logging and files for two copters

--- crazyflie_gazebo/scripts/test_high_level_circ2.py
+++ crazyflie_gazebo/scripts/test_high_level_circ2.py
@@ -13,15 +13,14 @@ from crazyflie_driver.msg import GenericLogData
 from crazyflie_driver.msg import Position
 
 timestr = time.strftime("%Y%m%d-%H%M%S")
-fp = open('sim3_' + timestr + '_.csv', 'w')
+fp = open('sim2_' + timestr + '_.csv', 'w')
 
 CX = 0.0
 CY = 0.0
 k = 5.0
 R = 0.5
 v_f = 0.5
-D_12 = 2*math.pi / 12
-D_23 = 2*math.pi / 12
+D = 2*math.pi / 2
 
 v_cruise = 0.5
 k_f = 1
@@ -55,15 +54,6 @@ def local_position_callback2(msg):
     current_position2.z = msg.values[2]
     current_position2.header = msg.header
 
-# local position callback [if need]
-def local_position_callback3(msg):
-    global current_position3
-    current_position3.x = msg.values[0]
-    current_position3.y = msg.values[1]
-    current_position3.z = msg.values[2]
-    current_position3.header = msg.header
-
-
 if __name__ == '__main__':
     rospy.init_node('test_high_level')
 
@@ -71,65 +61,52 @@ if __name__ == '__main__':
     current_position1 = Position()
     rospy.Subscriber("/cf1" + "/local_position" , GenericLogData , local_position_callback1)
     current_position2 = Position()
-    rospy.Subscriber("/cf2" + "/local_position" , GenericLogData , local_position_callback2)
-    current_position3 = Position()
-    rospy.Subscriber("/cf3" + "/local_position" , GenericLogData , local_position_callback3)
-    
+    rospy.Subscriber("/cf2" + "/local_position" , GenericLogData , local_position_callback2)    
 
     cf1 = crazyflie.Crazyflie("cf1", "/cf1")
     cf2 = crazyflie.Crazyflie("cf2", "/cf2")
-    cf3 = crazyflie.Crazyflie("cf3", "/cf3")
 
     cf1.setParam("commander/enHighLevel", 1)
     cf2.setParam("commander/enHighLevel", 1)
-    cf3.setParam("commander/enHighLevel", 1)
 
     cf1.takeoff(targetHeight = T_Z, duration = 2.0)
     cf2.takeoff(targetHeight = T_Z, duration = 2.0)
-    cf3.takeoff(targetHeight = T_Z, duration = 2.0)
     time.sleep(5.0)
 
     steps = 900
-    paramCalc = crazymath.Crazymath3(CX, CY, v_cruise, v_f, k_f, D_12, D_23, k, R)
+    paramCalc = crazymath.Crazymath2(CX, CY, v_cruise, v_f, k_f, D, k, R)
     for i in range(steps):
-        cf1_params, cf2_params, cf3_params, p_12, p_23 = paramCalc.calculate(current_position1.x, current_position1.y, current_position2.x, current_position2.y, current_position3.x, current_position3.y)
+        cf1_params, cf2_params, p_12 = paramCalc.calculate(current_position1.x, current_position1.y, current_position2.x, current_position2.y)
         
         setPx1 = current_position1.x + cf1_params.vx/5
         setPx2 = current_position2.x + cf2_params.vx/5
-        setPx3 = current_position3.x + cf3_params.vx/5
 
         setPy1 = current_position1.y + cf1_params.vy/5
         setPy2 = current_position2.y + cf2_params.vy/5
-        setPy3 = current_position3.y + cf3_params.vy/5
 
         if i == 0:
-            init_log(i=i, T_Z=T_Z, CX=CX, CY=CY, k=k, R=R, D_12=D_12, D_23=D_23,
-                     v_f=v_f, v_cruise=v_cruise, k_f=k_f, p_12=p_12, p_23=p_23,
+            init_log(i=i, T_Z=T_Z, CX=CX, CY=CY, k=k, R=R, D=D,
+                     v_f=v_f, v_cruise=v_cruise, k_f=k_f, p_12=p_12,
                      px_1=current_position1.x, py_1=current_position1.y, pz_1=current_position1.z, d_1=cf1_params.d, phi_1=cf1_params.phi, angle_1=cf1_params.angle, v1=cf1_params.v, vx1=cf1_params.vx, vy1=cf1_params.vy, setPx1=setPx1, setPy1=setPy1,
                      px_2=current_position2.x, py_2=current_position2.y, pz_2=current_position2.z, d_2=cf2_params.d, phi_2=cf2_params.phi, angle_2=cf2_params.angle, v2=cf2_params.v, vx2=cf2_params.vx, vy2=cf2_params.vy, setPx2=setPx2, setPy2=setPy2,
-                     px_3=current_position3.x, py_3=current_position3.y, pz_3=current_position3.z, d_3=cf3_params.d, phi_3=cf3_params.phi, angle_3=cf3_params.angle, v3=cf3_params.v, vx3=cf3_params.vx, vy3=cf3_params.vy, setPx3=setPx3, setPy3=setPy3,
                      )
 
-        write_log(i=i, T_Z=T_Z, CX=CX, CY=CY, k=k, R=R, D_12=D_12, D_23=D_23,
-                     v_f=v_f, v_cruise=v_cruise, k_f=k_f, p_12=p_12, p_23=p_23,
+        write_log(i=i, T_Z=T_Z, CX=CX, CY=CY, k=k, R=R, D=D,
+                     v_f=v_f, v_cruise=v_cruise, k_f=k_f, p_12=p_12,
                      px_1=current_position1.x, py_1=current_position1.y, pz_1=current_position1.z, d_1=cf1_params.d, phi_1=cf1_params.phi, angle_1=cf1_params.angle, v1=cf1_params.v, vx1=cf1_params.vx, vy1=cf1_params.vy, setPx1=setPx1, setPy1=setPy1,
                      px_2=current_position2.x, py_2=current_position2.y, pz_2=current_position2.z, d_2=cf2_params.d, phi_2=cf2_params.phi, angle_2=cf2_params.angle, v2=cf2_params.v, vx2=cf2_params.vx, vy2=cf2_params.vy, setPx2=setPx2, setPy2=setPy2,
-                     px_3=current_position3.x, py_3=current_position3.y, pz_3=current_position3.z, d_3=cf3_params.d, phi_3=cf3_params.phi, angle_3=cf3_params.angle, v3=cf3_params.v, vx3=cf3_params.vx, vy3=cf3_params.vy, setPx3=setPx3, setPy3=setPy3,
                      )
         
         cf1.goTo(goal = [setPx1, setPy1, T_Z], yaw=0.0, duration = 0.1, relative = False)
         cf2.goTo(goal = [setPx2, setPy2, T_Z], yaw=0.0, duration = 0.1, relative = False)
-        cf3.goTo(goal = [setPx3, setPy3, T_Z], yaw=0.0, duration = 0.1, relative = False)
 
         time.sleep(0.1)
 
     cf1.land(targetHeight = 0.0, duration = 2.0)
     cf2.land(targetHeight = 0.0, duration = 2.0)
-    cf3.land(targetHeight = 0.0, duration = 2.0)
     time.sleep(3.0)
 
     cf1.stop()
     cf2.stop()
-    cf3.stop()
 
     fp.close()
